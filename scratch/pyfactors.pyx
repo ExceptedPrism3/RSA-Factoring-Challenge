b0724633554b048dca1eccc6e5f24b4cbb0bf189
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-06-26 06:58:21 -0700

    RSA-Factoring-Challenge

--- scratch/pyfactors.pyx
+++
@@ -1,9 +0,0 @@
-cdef extern from "rsa.h":
-    long long unsigned int PollardRho(long long unsigned int n)
-
-def py_factors(n: bytes) -> None:
-    PollardRho(n)cdef extern from "rsa.h":
-    long long unsigned int PollardRho(long long unsigned int n)
-
-def py_factors(n: bytes) -> None:
-    PollardRho(n)
